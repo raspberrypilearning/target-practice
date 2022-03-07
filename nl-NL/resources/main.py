6b1d2be01f9a62efb6870b1e8198a6e1f7a6f1af
majamanojlovic <49232422+majamanojlovic@users.noreply.github.com>
2022-03-07 10:47:11 +0000

    New translations main.py (Dutch)

--- nl-NL/resources/main.py
+++ nl-NL/resources/main.py
@@ -1,61 +1,61 @@
 #!/bin/python3
 
-# Import library code
+# Bibliotheekcode importeren
 from p5 import *
 from math import *
 from random import randint
 
-# The mouse_pressed function goes here
-def mouse_pressed():
-  if hit_color == outer:  
-    print('You hit the outer circle, 50 points!') #Like functions, 'if' statements are indented
-  elif hit_color == inner:    
-    print('You hit the inner circle, 200 points!')   
-  elif hit_color == bullseye:    
-    print('You hit the bullseye, 500 points!')   
+# De muis_ingedrukt functie komt hier
+def muis_ingedrukt():
+  if raak_kleur == buitenste:  
+    print('Je raakt de buitenste cirkel, 50 punten!') #Net als functies, zijn 'if'-instructies ingesprongen
+  elif raak_kleur == binnenste:    
+    print('Je hebt de binnenste cirkel geraakt, 200 punten!')   
+  elif raak_kleur == roos:    
+    print('In de roos, 500 punten!')   
   else:   
-    print('You missed! No points!')    
+    print('Je hebt gemist! Geen punten!')    
     
-# The shoot_arrow function goes here
-def shoot_arrow():
-  global hit_color 
-  arrow_x = randint(100, 300)
-  arrow_y = randint(100, 300)
-  hit_color = get(arrow_x, arrow_y)
-  ellipse(arrow_x, arrow_y, 15, 15)
+# De schiet_pijl functie komt hier
+def schiet_pijl():
+  global raak_kleur 
+  pijl_x = randint(100, 300)
+  pijl_y = randint(100, 300)
+  raak_kleur = get(pijl_x, pijl_y)
+  ellipse(pijl_x, pijl_y, 15, 15)
 
 def setup():
-# Setup your game here
-  size(400, 400) # width and height
+# Stel je spel hier in
+  size(400, 400) # breedte en hoogte
   frame_rate(2)
 
 
 def draw():
-# Things to do in every frame
-  global outer, inner, bullseye
-  sky = color(92, 204, 206) # Red = 92, Green = 204, Blue = 206
-  grass = color(149, 212, 122)
-  wood = color(145, 96, 51)
-  outer = color(0, 120, 180) 
-  inner = color(210, 60, 60)
-  bullseye = color(220, 200, 0)
+# Te regelen in ieder frame
+  global buitenste, binnenste, roos
+  lucht = color(92, 204, 206) # Rood = 92, Groen = 204, Blauw = 206
+  gras = color(149, 212, 122)
+  hout = color(145, 96, 51)
+  buitenste = color(0, 120, 180) 
+  binnenste = color(210, 60, 60)
+  roos = color(220, 200, 0)
 
   no_stroke()
-  fill(sky)
+  fill(lucht)
   rect(0, 0, 400, 250)
-  fill(grass)
-  rect(0, 250, 400, 150)
+  fill(gras)
+  rect(0, 250, 400, 250)
   
-  fill(wood)
+  fill(hout)
   triangle(150, 350, 200, 150, 250, 350)
-  fill(outer)
+  fill(buitenste)
   ellipse(200, 200, 170, 170)
-  fill(inner)   
-  ellipse(200, 200, 110, 110) #Inner circle   
-  fill(bullseye)   
-  ellipse(200, 200, 30, 30) #Bullseye 
+  fill(binnenste)   
+  ellipse(200, 200, 110, 110) #Binnenste cirkel   
+  fill(roos)   
+  ellipse(200, 200, 30, 30) #Roos 
   
-  fill(wood)
-  shoot_arrow()
-# Keep this to run your code
+  fill(hout)
+  schiet_pijl()
+# Bewaar dit om je code uit te voeren
 run()
