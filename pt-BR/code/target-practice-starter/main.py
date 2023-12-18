b9c3141cc4181defa51ea132f14d1e407e09809a
Sasha Mishcheriakova <135987917+sashamishcheriakova@users.noreply.github.com>
2023-12-18 11:20:16 +0000

    New translations main.py (Portuguese, Brazilian)

---
+++ pt-BR/code/target-practice-starter/main.py
@@ -0,0 +1,19 @@
+# Import library code
+from p5 import *
+from random import randint
+
+# The mouse_pressed function goes here
+
+# The shoot_arrow function goes here
+
+def setup():
+    # Setup your game here
+    size(400, 400)  # width and height of screen
+
+def draw():
+    # Things to do in every frame
+    fill('cyan')  # Set the fill color for the sky to cyan
+    rect(0, 0, 400, 250)  # Draw a rectangle for the sky with these values for x, y, width, height
+
+# Keep this to run your code
+run(frame_rate=2)
