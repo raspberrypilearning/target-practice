e2734bbcc6fcffef1984283fb91b076238cd44f9
Sasha Mishcheriakova <135987917+sashamishcheriakova@users.noreply.github.com>
2024-02-14 15:47:38 +0000

    New translations main.py (French)

--- fr-FR/code/target-practice-solution/main.py
+++ fr-FR/code/target-practice-solution/main.py
@@ -1,47 +1,47 @@
-# Import library code!
+# importation des librairies de code !
 from p5 import *
 from random import randint
 
-# The mouse_pressed function goes here
-def mouse_pressed():
-    if hit_colour == Color('blue').hex:  # Like functions, 'if' statements are indented
-        print('You hit the outer circle, 50 points!')
-    elif hit_colour == Color('red').hex:
-        print('You hit the inner circle, 200 points!')
-    elif hit_colour == Color('yellow').hex:
-        print('You hit the middle, 500 points!')
+# La fonction souris_pressee vient ici
+def souris_pressee():
+    if touche_couleur == Color('blue').hex: # Comme les fonctions, les instructions 'if' sont indentées
+        print('Tu as touché le cercle extérieur, 50 points !')
+    elif touche_couleur == Color('red').hex:
+        print('Tu as touché le cercle intérieur, 200 points !')
+    elif touche_couleur == Color('yellow').hex:
+        print('Tu as touché le centre, 500 points !')
     else:
-        print('You missed! No points!')
+        print('Tu as loupé la cible ! Aucun point !')
 
-# The shoot_arrow function goes here
-def shoot_arrow():
-    global hit_colour  # Can be used in other functions
-    arrow_x = randint(100, 300)  # Store a random number between 100 and 300
-    arrow_y = randint(100, 300)  # Store a random number between 100 and 300
-    hit_colour = get(arrow_x, arrow_y).hex  # Get the hit colour
-    fill('sienna')  # Set the arrow to fill colour to brown
-    circle(arrow_x, arrow_y, 15)  # Draw a small circle at random coordinates
+# La fonction tire_fleche vient ici
+def tire_fleche():
+    global touche_couleur # Peut être utilisé dans d'autres fonctions
+    fleche_x = randint(100, 300) # Stocke un nombre aléatoire entre 100 et 300
+    fleche_y = randint(100, 300) # Stocke un nombre aléatoire entre 100 et 300
+    touche_couleur = get(fleche_x, fleche_y).hex # Récupère la couleur de l'endroit touché
+    fill('sienna') # Définit la flèche pour remplir la couleur sur marron
+    circle(fleche_x, fleche_y, 15) # Dessine un petit cercle à des coordonnées aléatoires
 
 def setup():
-    # Setup your game here
-    size(400, 400)  # width and height
+    # Configure ton jeu ici
+    size(400, 400) # largeur et hauteur
     no_stroke()
 
 def draw():
-    # Things to do in every frame
+    # Choses à faire dans chaque image
     fill('cyan')
-    rect(0, 0, 400, 250)  # Sky
+    rect(0, 0, 400, 250) # Ciel
     fill('lightgreen')
-    rect(0, 250, 400, 150)  # Grass
+    rect(0, 250, 400, 150) # Herbe
     fill('sienna')
-    triangle(150, 350, 200, 150, 250, 350)  # Stand
+    triangle(150, 350, 200, 150, 250, 350) # Support
     fill('blue')
-    circle(200, 200, 170)  # Outer circle
+    circle(200, 200, 170)  # Cercle extérieur
     fill('red')
-    circle(200, 200, 110)  # Inner circle
+    circle(200, 200, 110)  # Cercle intérieur
     fill('yellow')
-    circle(200, 200, 30)  # Middle circle
-    shoot_arrow()
+    circle(200, 200, 30)  # Cercle du milieu
+    tire_fleche()
 
-# Keep this to run your code
+# Garde ceci pour exécuter ton code
 run(frame_rate=2)
