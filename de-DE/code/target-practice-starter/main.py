8e75dad0e03f982ec561fb78df277c4e61d89844
Sasha Mishcheriakova <135987917+sashamishcheriakova@users.noreply.github.com>
2024-05-22 12:37:07 +0100

    New translations main.py (German)

--- de-DE/code/target-practice-starter/main.py
+++ de-DE/code/target-practice-starter/main.py
@@ -7,12 +7,12 @@ from random import randint
 # Die Funktion „schiess_pfeil“ kommt hierher
 
 def setup():
-    # Set up your game here
+    # Richte hier Dein Spiel ein
     size(400, 400) # Breite und Höhe
 
 def draw():
     # Dinge die in jedem Frame passieren
-    fill('cyan')  # Set the fill colour for the sky to cyan
+    fill('cyan') # Setzt die Füllfarbe für den Himmel auf Cyan
     rect(0, 0, 400, 250) # Zeichnet ein Rechteck für den Himmel mit diesen Werten für x, y, Breite, Höhe
 
 # Lass dies so stehen, um Deinen Code auszuführen
