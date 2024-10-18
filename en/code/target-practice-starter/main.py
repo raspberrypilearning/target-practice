189310c260e7ca65d5a10415cd89f3b07793af98
Laura Sach <5183697+lawsie@users.noreply.github.com>
2024-10-18 11:40:14 +0100

    Update first step

--- en/code/target-practice-starter/main.py
+++ en/code/target-practice-starter/main.py
@@ -1,19 +1,23 @@
-# Import library code
+## Import library code
 from p5 import *
 from random import randint
 
 # The mouse_pressed function goes here
 
+
 # The shoot_arrow function goes here
 
+
 def setup():
-    # Set up your game here
-    size(400, 400)  # width and height of screen
+    # Setup your game here
+    size(400, 400)
+
 
 def draw():
     # Things to do in every frame
-    fill('cyan')  # Set the fill colour for the sky to cyan
-    rect(0, 0, 400, 250)  # Draw a rectangle for the sky with these values for x, y, width, height
+    fill("cyan")
+    rect(0, 0, 400, 250)
+
 
 # Keep this to run your code
 run(frame_rate=2)
