b52076c3c1b2b929dd9fa95d7fc5909710398f3e
Sasha Mishcheriakova <135987917+sashamishcheriakova@users.noreply.github.com>
2025-02-21 12:14:53 +0000

    New translations main.py (German)

--- de-DE/code/target-practice-starter/main.py
+++ de-DE/code/target-practice-starter/main.py
@@ -4,16 +4,21 @@ from random import randint
 
 # Die Funktion „mouse_pressed“ kommt hierher
 
+
 # Die Funktion „schiess_pfeil“ kommt hierher
+
 
 def setup():
     # Richte hier Dein Spiel ein
     size(400, 400) # Breite und Höhe
+    no_stroke()
+
 
 def draw():
     # Dinge die in jedem Frame passieren
-    fill('cyan') # Setzt die Füllfarbe für den Himmel auf Cyan
-    rect(0, 0, 400, 250) # Zeichnet ein Rechteck für den Himmel mit diesen Werten für x, y, Breite, Höhe
+    fill("cyan")
+    rect(0, 0, 400, 250)
+
 
 # Lass dies so stehen, um Deinen Code auszuführen
 run(frame_rate=2)
