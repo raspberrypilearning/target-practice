8dd5f7550a621f753e7d60c2e0340c586974a4f2
Sasha Mishcheriakova <135987917+sashamishcheriakova@users.noreply.github.com>
2025-02-21 12:14:51 +0000

    New translations main.py (French)

--- fr-FR/code/target-practice-starter/main.py
+++ fr-FR/code/target-practice-starter/main.py
@@ -4,16 +4,21 @@ from random import randint
 
 # La fonction souris_pressee vient ici
 
+
 # La fonction tire_fleche vient ici
+
 
 def setup():
     # Configure ton jeu ici
     size(400, 400) # largeur et hauteur de l'écran
+    no_stroke()
+
 
 def draw():
     # Choses à faire dans chaque image
-    fill('cyan') # Définit la couleur de remplissage du ciel sur cyan
+    fill("cyan")
     rect(0, 0, 400, 250) # Dessine un rectangle pour le ciel avec ces valeurs pour x, y, largeur, hauteur
+
 
 # Garde ceci pour exécuter ton code
 run(frame_rate=2)
