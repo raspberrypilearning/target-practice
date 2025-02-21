779b70b5ac1bf238074ddbfd7d3a0fce1212cd77
Sasha Mishcheriakova <135987917+sashamishcheriakova@users.noreply.github.com>
2025-02-21 12:14:55 +0000

    New translations main.py (Italian)

--- it-IT/code/target-practice-starter/main.py
+++ it-IT/code/target-practice-starter/main.py
@@ -4,16 +4,21 @@ from random import randint
 
 # La funzione mouse_pressed va qui
 
+
 # La funzione shoot_arrow va qui
+
 
 def setup():
     # Imposta il tuo gioco qui
     size(400, 400)  # larghezza e altezza dello schermo
+    no_stroke()
+
 
 def draw():
     # Cose da fare in ogni fotogramma
-    fill('cyan') # Imposta il colore di riempimento del cielo su ciano
-    rect(0, 0, 400, 250) # Disegna un rettangolo per il cielo con questi valori per x, y, larghezza, altezza
+    fill("cyan")
+    rect(0, 0, 400, 250)
+
 
 # Conserva questa parte per eseguire il codice
 run(frame_rate=2)
