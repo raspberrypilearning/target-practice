194cd77bc6fede932ebd3ea96e4dc697228e68fc
Sasha Mishcheriakova <135987917+sashamishcheriakova@users.noreply.github.com>
2025-02-21 12:15:03 +0000

    New translations main.py (Portuguese, Brazilian)

--- pt-BR/code/target-practice-starter/main.py
+++ pt-BR/code/target-practice-starter/main.py
@@ -4,16 +4,21 @@ from random import randint
 
 # A função mouse_pressed vai aqui
 
+
 # A função atirar_flecha vai aqui
+
 
 def setup():
     # Configure seu jogo aqui
     size(400, 400)  # largura e altura da tela
+    no_stroke()
+
 
 def draw():
     # Coisas para fazer em cada quadro
-    fill('cyan') # Defina a cor de preenchimento do céu como ciano
-    rect(0, 0, 400, 250) # Desenhe um retângulo para o céu com estes valores para x, y, largura, altura
+    fill("cyan")
+    rect(0, 0, 400, 250)
+
 
 # Mantenha isto para executar seu código
 run(frame_rate=2)
