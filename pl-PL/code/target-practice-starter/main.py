cc15aa1b22ee46fcf8ea28f2374e194b5143b941
Sasha Mishcheriakova <135987917+sashamishcheriakova@users.noreply.github.com>
2025-02-21 12:14:59 +0000

    New translations main.py (Polish)

--- pl-PL/code/target-practice-starter/main.py
+++ pl-PL/code/target-practice-starter/main.py
@@ -1,19 +1,24 @@
-# Importuj kod biblioteki
+## Import library code
 from p5 import *
 from random import randint
 
 # Funkcja mouse_pressed pojawi się tutaj
 
+
 # Funkcja shoot_arrow pojawia się tutaj
+
 
 def setup():
     # Set up your game here
-    rozmiar(400, 400) # szerokość i wysokość ekranu
+    size(400, 400)
+    no_stroke()
+
 
 def draw():
     # Rzeczy do zrobienia w każdej klatce
-    fill('cyan')  # Set the fill colour for the sky to cyan
-    Rect(0, 0, 400, 250) # Narysuj prostokąt nieba za pomocą tych wartości dla x, y, width, height
+    fill("cyan")
+    rect(0, 0, 400, 250)
+
 
 # Zatrzymaj to, aby uruchomić swój kod
 run(frame_rate=2)
