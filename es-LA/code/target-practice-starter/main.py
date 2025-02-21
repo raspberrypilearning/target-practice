1ef46d0790c189af3da5c00df8971fa6174d0be5
Sasha Mishcheriakova <135987917+sashamishcheriakova@users.noreply.github.com>
2025-02-21 12:15:09 +0000

    New translations main.py (Spanish, Latin America)

--- es-LA/code/target-practice-starter/main.py
+++ es-LA/code/target-practice-starter/main.py
@@ -1,19 +1,24 @@
-# Import library code
+## Import library code
 from p5 import *
 from random import randint
 
 # The mouse_pressed function goes here
 
+
 # The shoot_arrow function goes here
+
 
 def setup():
     # Configura tu juego aquí
-    size(400, 400)  # width and height of screen
+    size(400, 400)
+    no_stroke()
+
 
 def draw():
     # Things to do in every frame
-    fill('cyan') # Establece el color de relleno del cielo en cian
-    rect(0, 0, 400, 250)  # Draw a rectangle for the sky with these values for x, y, width, height
+    fill("cyan")
+    rect(0, 0, 400, 250)
+
 
 # Keep this to run your code
 run(frame_rate=2)
